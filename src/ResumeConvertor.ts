fab31c9f36399e1d411f84b862cc5aedd926fc39
corentin <corentinf@free.fr>
2025-08-06 14:34:43 +0200

    full fonctional composite project

--- src/ResumeConvertor.ts
+++ src/ResumeConvertor.ts
@@ -1,11 +1,17 @@
+import { promises as fs } from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
 import { ResumeGenerator } from "./components/resume.js";
 import { ResumeSchema } from "../../json_cv_schema/src/type/type.js";
-import * as fs from 'fs/promises';
 
 export default class ResumeConvertor {
     public async generateResume(resumeJson: ResumeSchema) {
-        const css = await fs.readFile("resource/page.css");
-        const js = await fs.readFile("resource/page.js");
+        const __filename = fileURLToPath(import.meta.url);
+        const _srcPath = path.dirname(__filename);
+        const _projectPath = path.dirname(_srcPath);
+        const css = await fs.readFile(path.join(_projectPath, "resource/page.css"));
+        const js = await fs.readFile(path.join(_projectPath, "resource/page.js"));
         return ResumeGenerator.generate(resumeJson, css, js);
     }
 }
