34267da45df461dd5e6203f054d45616c3dd3670
corentin <corentinf@free.fr>
2025-08-25 14:52:46 +0200

    remove dist-tests

--- dist-tests/src/utils/IconGenerator.js
+++
@@ -1,7 +0,0 @@
-import feather from 'feather-icons';
-export default class IconGenerator {
-    static generate(name, fallback = 'anchor') {
-        const icon = feather.icons[name] || (fallback && feather.icons[fallback]);
-        return icon?.toSvg({ width: 16, height: 16 });
-    }
-}
--- dist-tests/src/utils/LinkGenerator.js
+++
@@ -1,8 +0,0 @@
-export default class LinkGenerator {
-    static generate(url, name) {
-        return name ? (url ? `<a href="${url}">${name}</a>` : name) : url ? `<a href="${url}">${LinkGenerator.formatURL(url)}</a>` : '';
-    }
-    static formatURL(url) {
-        return url.replace(/^(https?:|)\/\//, '').replace(/\/$/, '');
-    }
-}
